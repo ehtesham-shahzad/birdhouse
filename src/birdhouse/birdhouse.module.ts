2a4581fa6e9b3914c762953a0eeb67c718ff900d
Ehtesham Shahzad <shahzad@epacken.com>
2023-08-07 22:58:25 +0400

    Added missing provider in birdhouse module file

--- src/birdhouse/birdhouse.module.ts
+++ src/birdhouse/birdhouse.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { BirdhouseController } from './birdhouse.controller';
+import { BirdhouseRepository } from './birdhouse.repository';
 import { BirdhouseService } from './birdhouse.service';
 import { Birdhouse } from './entities/birdhouse.entity';
 import { ResidenceHistory } from './entities/residence-history.entity';
@@ -8,6 +9,6 @@ import { ResidenceHistory } from './entities/residence-history.entity';
 @Module({
   imports: [TypeOrmModule.forFeature([Birdhouse, ResidenceHistory])],
   controllers: [BirdhouseController],
-  providers: [BirdhouseService]
+  providers: [BirdhouseService, BirdhouseRepository]
 })
 export class BirdhouseModule { }
