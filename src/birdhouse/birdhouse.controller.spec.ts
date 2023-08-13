4070e843968c99b953f70d6cfd88044a767baeb7
Ehtsham <ehtsham@Dawoods-MacBook-Pro.local>
2023-08-13 17:31:43 +0400

    Wrote test cases

--- src/birdhouse/birdhouse.controller.spec.ts
+++ src/birdhouse/birdhouse.controller.spec.ts
@@ -1,20 +1,164 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { BirdhouseController } from './birdhouse.controller';
 import { BirdhouseService } from './birdhouse.service';
+import { BirdhouseRepository } from './repositories/birdhouse.repository';
+import { ResidenceHistoryRepository } from './repositories/residenceHistory.repository';
+import { v4 as uuidv4 } from 'uuid';
+import { CreateBirdhouseRequestDto } from './dto/create-birdhouse-request.dto';
+import { UpdateBirdhouseDto } from './dto/update-birdhouse.dto';
+import { UpdateOccupancyDto } from './dto/update-occupancy.dto';
+import { Birdhouse } from './entities/birdhouse.entity';
+import { ResidenceHistory } from './entities/residence-history.entity';
 
 describe('BirdhouseController', () => {
   let controller: BirdhouseController;
+  let service: BirdhouseService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [BirdhouseController],
-      providers: [BirdhouseService],
+      providers: [BirdhouseService,
+        {
+          provide: BirdhouseRepository,
+          useValue: {},
+        },
+        {
+          provide: ResidenceHistoryRepository,
+          useValue: {},
+        }
+      ],
     }).compile();
 
     controller = module.get<BirdhouseController>(BirdhouseController);
+    service = module.get<BirdhouseService>(BirdhouseService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
   });
+
+  describe('create', () => {
+    it('should create and return a new birdhouse', async () => {
+      const mockCreateBirdhouseDto: CreateBirdhouseRequestDto = {
+        name: 'Twitter',
+        latitude: 12.344,
+        longitude: 31.122
+      };
+
+      service.create = jest.fn().mockResolvedValue({ id: 1, ...mockCreateBirdhouseDto });
+
+      const result = await controller.create(mockCreateBirdhouseDto);
+
+      expect(result).toEqual({ id: 1, ...mockCreateBirdhouseDto });
+      expect(service.create).toHaveBeenCalledWith(mockCreateBirdhouseDto);
+    });
+  });
+
+  describe('createInBulk', () => {
+    it('should create birdhouses in bulk and return the results', async () => {
+      const mockCreateBirdhousesDto: CreateBirdhouseRequestDto[] = [
+        {
+          name: 'Twitter',
+          latitude: 12.344,
+          longitude: 31.122
+        },
+        {
+          name: 'X',
+          latitude: 12.344,
+          longitude: 31.122
+        }
+      ];
+
+      service.createInBulk = jest.fn().mockResolvedValue([{ id: 1, ...mockCreateBirdhousesDto[0] }]);
+
+      const result = await controller.createInBulk(mockCreateBirdhousesDto);
+
+      expect(result).toEqual([{ id: 1, ...mockCreateBirdhousesDto[0] }]);
+      expect(service.createInBulk).toHaveBeenCalledWith(mockCreateBirdhousesDto);
+    });
+  });
+
+  describe('update', () => {
+    it('should update a birdhouse and return the updated data', async () => {
+      const mockUbid = 'test-ubid';
+      const mockUpdateBirdhouseDto: UpdateBirdhouseDto = {
+        name: 'Twitter',
+        longitude: 12.333,
+        latitude: 11.332,
+        updatedAt: new Date()
+      };
+
+      service.update = jest.fn().mockResolvedValue({ id: 1, ...mockUpdateBirdhouseDto });
+
+      const result = await controller.update(mockUbid, mockUpdateBirdhouseDto);
+
+      expect(result).toEqual({ id: 1, ...mockUpdateBirdhouseDto });
+      expect(service.update).toHaveBeenCalledWith(mockUbid, mockUpdateBirdhouseDto);
+    });
+  });
+
+  describe('updateOccupancy', () => {
+    it('should update birdhouse occupancy and return the updated data', async () => {
+      const mockUbid = 'test-ubid';
+      const mockUpdateOccupancyDto: UpdateOccupancyDto = {
+        updatedAt: new Date(),
+        birds: 3,
+        eggs: 8
+      };
+
+      service.updateOccupancy = jest.fn().mockResolvedValue({ id: 1, ...mockUpdateOccupancyDto });
+
+      const result = await controller.updateOccupancy(mockUbid, mockUpdateOccupancyDto);
+
+      expect(result).toEqual({ id: 1, ...mockUpdateOccupancyDto });
+      expect(service.updateOccupancy).toHaveBeenCalledWith(mockUbid, mockUpdateOccupancyDto);
+    });
+  });
+
+  describe('findOne', () => {
+    it('should find and return a birdhouse', async () => {
+      const mockUbid = uuidv4();
+      const mockBirdhouse = { id: uuidv4(), name: 'Test Birdhouse' };
+
+      service.findOne = jest.fn().mockResolvedValue(mockBirdhouse);
+
+      const result = await controller.findOne(mockUbid);
+
+      expect(result).toEqual(mockBirdhouse);
+      expect(service.findOne).toHaveBeenCalledWith(mockUbid);
+    });
+  });
+
+  describe('residenceHistory', () => {
+    it('should get residence history of a birdhouse', async () => {
+      const mockUbid = uuidv4();
+      const mockResidenceHistory = {
+        birdhouse: {
+          id: uuidv4(),
+          latitude: 21.344,
+          longitude: 12.443,
+          name: 'Twitter',
+          ubid: mockUbid,
+        },
+        residenceHistory: [
+          {
+            id: uuidv4(),
+            eggs: 3,
+            birds: 1
+          }
+        ]
+      } as {
+        birdhouse: Birdhouse;
+        residenceHistory: ResidenceHistory[];
+      };
+
+      service.residenceHistory = jest.fn().mockResolvedValue(mockResidenceHistory);
+
+      const result = await controller.residenceHistory(mockUbid);
+
+      expect(result).toEqual(mockResidenceHistory);
+      expect(service.residenceHistory).toHaveBeenCalledWith(mockUbid);
+    });
+  });
+
 });
--- src/birdhouse/birdhouse.service.spec.ts
+++ src/birdhouse/birdhouse.service.spec.ts
@@ -1,18 +1,110 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { BirdhouseService } from './birdhouse.service';
+import { BirdhouseRepository } from './repositories/birdhouse.repository';
+import { ResidenceHistoryRepository } from './repositories/residenceHistory.repository';
+import { CreateBirdhouseRequestDto } from './dto/create-birdhouse-request.dto';
+import { CreateBirdhouseResponseDto } from './dto/create-birdhouse-response.dto';
+import { v4 as uuidv4 } from 'uuid';
+import { assert } from 'console';
+import { Birdhouse } from './entities/birdhouse.entity';
+import { ResidenceHistory } from './entities/residence-history.entity';
 
 describe('BirdhouseService', () => {
-  let service: BirdhouseService;
+  let birdhouseService: BirdhouseService;
+  let birdhouseRepository: BirdhouseRepository;
+  let residenceHistoryRepository: ResidenceHistoryRepository;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [BirdhouseService],
+      providers: [
+        BirdhouseService,
+        {
+          provide: BirdhouseRepository,
+          useValue: {},
+        },
+        {
+          provide: ResidenceHistoryRepository,
+          useValue: {},
+        },
+      ],
     }).compile();
 
-    service = module.get<BirdhouseService>(BirdhouseService);
+    birdhouseService = module.get<BirdhouseService>(BirdhouseService);
+    birdhouseRepository = module.get<BirdhouseRepository>(BirdhouseRepository);
+    residenceHistoryRepository = module.get<ResidenceHistoryRepository>(ResidenceHistoryRepository);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+
+  describe('createBirdhouseObj', () => {
+    it('should create a birdhouse object with the given dto', () => {
+      const mockDto = new CreateBirdhouseRequestDto();
+      mockDto.name = 'Test Birdhouse';
+      mockDto.longitude = 10.0;
+      mockDto.latitude = 20.0;
+      const birdhouse = birdhouseService.createBirdhouseObj(mockDto);
+
+      expect(birdhouse.name).toBe(mockDto.name);
+      expect(birdhouse.longitude).toBe(mockDto.longitude);
+      expect(birdhouse.latitude).toBe(mockDto.latitude);
+
+      expect(birdhouse.id).toBeDefined();
+      expect(birdhouse.ubid).toBeDefined();
+    });
   });
+
+  describe('createResidenceHistoryObj', () => {
+
+    it('should create a residence history object with birds and eggs', () => {
+      const birdHouseId = uuidv4();
+      const birds = 5;
+      const eggs = 5;
+
+      const residenceHistory = birdhouseService.createResidenceHistoryObj(birdHouseId, birds, eggs);
+      expect(residenceHistory.birdHouseId).toBe(birdHouseId);
+      expect(residenceHistory.birds).toBe(birds);
+      expect(residenceHistory.eggs).toBe(eggs);
+    });
+
+    it('should create a residence history object', () => {
+      const birdHouseId = uuidv4();
+
+      const residenceHistory = birdhouseService.createResidenceHistoryObj(birdHouseId);
+      expect(residenceHistory.birdHouseId).toBe(birdHouseId);
+      expect(residenceHistory.birds).toBe(0);
+      expect(residenceHistory.eggs).toBe(0);
+    });
+
+  });
+
+  describe('birdhouseResponse', () => {
+    it('it should create a response from Birdhouse and ResidenceHistory objects', () => {
+
+      const mockBirdhouse = {
+        id: uuidv4(),
+        ubid: uuidv4(),
+        name: 'Twitter',
+        latitude: 21.234,
+        longitude: 21.222,
+      } as Birdhouse;
+
+      const mockResidenceHistory = {
+        birdHouseId: uuidv4(),
+        birds: 2,
+        eggs: 1
+      } as ResidenceHistory;
+
+      const birdhouseResponse = birdhouseService.birdhouseResponse(mockBirdhouse, mockResidenceHistory);
+
+      expect(birdhouseResponse).toBeDefined();
+      expect(birdhouseResponse.name).toBe('Twitter');
+      expect(birdhouseResponse.id).toBeDefined();
+      expect(birdhouseResponse.ubid).toBeDefined();
+      expect(birdhouseResponse.eggs).toBe(1);
+      expect(birdhouseResponse.birds).toBe(2);
+      expect(birdhouseResponse.latitude).toBe(21.234);
+      expect(birdhouseResponse.longitude).toBe(21.222);
+
+    });
+  });
+
 });
